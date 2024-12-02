7cd3a320fceb7b8d13cc31664c2cf21cd3521aae
kira0722 <samuel_quirama23211@elpoli.edu.co>
2024-12-01 21:03:38 -0500

    subiendo cuerpo del proyecto, mas la conexion con la BD

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('error en la conexion');
+    return;
+  }
+  console.log('conexion exitosa');
+})
+
